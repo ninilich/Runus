5161900bbcd39f713934584f1c7846ac08cde437
ninilich <65906651+ninilich@users.noreply.github.com>
2021-01-02 19:35:12 +0300

    add the content of the refs

---
+++ postgres/ref_entity_type.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.ref_entity_type (entity_type_id,entity_type_name) VALUES 
+('ORG','Организация')
+,('PER','Персона')
+,('LOC','Локация')
+;
---
+++ postgres/ref_news_src.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.ref_news_src (news_src_id,news_src_name) VALUES 
+('EHO','Эхо Москвы')
+,('RBK','РБК')
+,('RIA','РИА Новости')
+,('YEC','Яндекс.Экономика')
+,('YPL','Яндекс.Политика')
+,('LNT','Лента.ру')
+;
